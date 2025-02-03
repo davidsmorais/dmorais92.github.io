7be952634f963071c09fe6c599b603bb07acc02d
David  Morais <davidmorais92@gmail.com>
2025-02-03 16:41:21 +0000

    🚧wip: setting up next and shadcn
        by David Morais <david@davidmorais.com>

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+export default function Page() {
+	return (
+		<body className="bg-primary">
+			<div className="m-5">
+				<h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+					The People of the Kingdom
+				</h2>
+			</div>
+		</body>
+	);
+}
