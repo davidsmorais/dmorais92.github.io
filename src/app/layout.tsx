c572f1a7e7e96ac9fc616e8aeb9bdccf360bbba8
David  Morais <davidmorais92@gmail.com>
2025-02-03 16:46:41 +0000

    🚀feature: next js with dark theme and shadcn
        by David Morais <david@davidmorais.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,23 @@
-import { ReactNode } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function RootLayout({
 	children,
-}: {
-	children: ReactNode;
-}) {
+}: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
+		<>
+			<html lang="en" suppressHydrationWarning>
+				<head />
+				<body>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						{children}
+					</ThemeProvider>
+				</body>
+			</html>
+		</>
 	);
 }
