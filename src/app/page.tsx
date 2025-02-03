f815336a49110e74337741c8bda5a4c44f6ae818
David  Morais <davidmorais92@gmail.com>
2025-02-03 19:40:36 +0000

    🚀feature: neon gradient card
        by David Morais <david@davidmorais.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { ModeToggle } from "@/components/ThemeToggle";
-import { TypographyH3 } from "@/components/Typo";
+import { TypographyH1, TypographyH3 } from "@/components/Typo";
+import { NeonGradientCard } from "@/components/ui/neon-card";
 import { useTranslations } from "next-intl";
 export default function Page() {
 	const t = useTranslations();
@@ -7,7 +8,13 @@ export default function Page() {
 		<body className="bg-secondary">
 			<main className="m-5 p-2">
 				<ModeToggle />
-				<TypographyH3>{t("meTitle")}</TypographyH3>
+				<NeonGradientCard
+					neonColors={{ firstColor: "#66e", secondColor: "#6ea" }}
+				>
+					<TypographyH1 className="pointer-events-none z-10 h-full whitespace-pre-wrap bg-gradient-to-br from-[#66e] from-25% to-[#6ea] bg-clip-text text-center text-6xl font-bold leading-none tracking-tighter text-transparent dark:drop-shadow-[0_5px_5px_rgba(0,0,0,0.8)]">
+						{t("meTitle")}
+					</TypographyH1>
+				</NeonGradientCard>
 			</main>
 		</body>
 	);
