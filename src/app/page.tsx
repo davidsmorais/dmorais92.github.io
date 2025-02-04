5664ac6f6aa29b18429817c4bb2907c75ed9f217
David  Morais <davidmorais92@gmail.com>
2025-02-04 12:25:58 +0000

    🚀feature: fade in animation
        by David Morais <david@davidmorais.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import FlexDiv from "@/components/FlexDiv";
 import { ModeToggle } from "@/components/ThemeToggle";
-import { TypographyH1, TypographyH3 } from "@/components/Typo";
+
+import { TypographyH1 } from "@/components/Typo";
 import { NeonGradientCard } from "@/components/ui/neon-card";
 import { RetroGrid } from "@/components/ui/retro-grid";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import { Fade } from "react-awesome-reveal";
 
 export default function Page() {
 	const t = useTranslations();
@@ -13,21 +15,24 @@ export default function Page() {
 			<RetroGrid darkLineColor="#6ea" lightLineColor="#ed199f" />
 			<body className="bg-secondary min-h-svh max-w-svw">
 				<main className="m-5 p-2">
-					<NeonGradientCard
-						neonColors={{ firstColor: "#66e", secondColor: "#6ea" }}
-					>
-						<FlexDiv>
-							<TypographyH1 className="whitespace-pre-wrap" gradient>
-								{t("meTitle")}
-							</TypographyH1>
-							<Image
-								src="/img/avatar.png"
-								width={200}
-								height={200}
-								alt="Picture of the author"
-							/>
-						</FlexDiv>
-					</NeonGradientCard>
+					<Fade cascade damping={0.5}>
+						<NeonGradientCard
+							className="animate-fade-up"
+							neonColors={{ firstColor: "#66e", secondColor: "#6ea" }}
+						>
+							<FlexDiv>
+								<TypographyH1 className="whitespace-pre-wrap" gradient>
+									{t("meTitle")}
+								</TypographyH1>
+								<Image
+									src="/img/avatar.png"
+									width={200}
+									height={200}
+									alt="Picture of the author"
+								/>
+							</FlexDiv>
+						</NeonGradientCard>
+					</Fade>
 					<ModeToggle />
 				</main>
 			</body>
