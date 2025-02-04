aaf9064a99da370de2c82d403b889656951c3401
David  Morais <davidmorais92@gmail.com>
2025-02-04 16:26:48 +0000

    ✨polish: localize & add links to nav bar
        by David Morais <david@davidmorais.com>

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import * as TabsPrimitive from "@radix-ui/react-tabs";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
 import * as React from "react";
 
 import FlexDiv from "@/components/FlexDiv";
@@ -10,24 +12,35 @@ import { Button } from "./button";
 export const Navbar = React.forwardRef<
 	React.ElementRef<typeof TabsPrimitive.List>,
 	React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
-	<nav
-		ref={ref}
-		className={cn(
-			"bg-gradient-to-r shadow-sm from-neutral-950/90 via-neutral-900/75 to-neutral-950/80",
-			"h-15  inline-flex items-center justify-center w-full  positon-sticky top-0 p-1 text-muted-foreground  border-b-neutral-300 border-b-2 py-2  fixed",
-			className,
-		)}
-		{...props}
-	>
-		<FlexDiv justify="start" size={1} gap={"0.25rem"}>
-			<Button variant="ghost">Home</Button>
-			<Button variant="ghost">Projects</Button>
-			<Button variant="ghost">Blog</Button>
-			<Button variant="ghost">Resume</Button>
-			<Button>Get In Touch</Button>
-		</FlexDiv>
-		<FlexDiv>{props.children}</FlexDiv>
-	</nav>
-));
+>(({ className, ...props }, ref) => {
+	const t = useTranslations();
+	return (
+		<nav
+			ref={ref}
+			className={cn(
+				"bg-gradient-to-r shadow-sm from-neutral-950/90 via-neutral-900/75 to-neutral-950/80",
+				"h-15  inline-flex items-center justify-center w-full  positon-sticky top-0 p-1 text-muted-foreground  border-b-neutral-300 border-b-2 py-2  fixed",
+				className,
+			)}
+			{...props}
+		>
+			<FlexDiv justify="start" size={1} gap={"0.5rem"} mx="5rem">
+				<Button variant="link">
+					<Link href="/">{t("nav.home")}</Link>
+				</Button>
+				<Button variant="link">
+					<Link href="/projects">{t("nav.projects")}</Link>
+				</Button>
+				<Button variant="link">
+					<Link href="/blog">{t("nav.blog")}</Link>
+				</Button>
+				<Button variant="link">
+					<Link href="/resume">{t("nav.resume")}</Link>
+				</Button>
+				<Button>{t("nav.contact")}</Button>
+			</FlexDiv>
+			<FlexDiv mx="2.5rem">{props.children}</FlexDiv>
+		</nav>
+	);
+});
 Navbar.displayName = "Navbar";
