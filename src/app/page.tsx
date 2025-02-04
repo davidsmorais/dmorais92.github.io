7cbce10dde170591fed7823511578d74173547f5
David  Morais <davidmorais92@gmail.com>
2025-02-04 13:54:31 +0000

    🚀feature: replace neon gradient card with custom made
        by David Morais <david@davidmorais.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import FlexDiv from "@/components/FlexDiv";
 import { ModeToggle } from "@/components/ThemeToggle";
 
 import { TypographyH1 } from "@/components/Typo";
-import { NeonGradientCard } from "@/components/ui/neon-card";
+import { Card, CardContent } from "@/components/ui/card";
+
 import { RetroGrid } from "@/components/ui/retro-grid";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
@@ -12,26 +13,25 @@ export default function Page() {
 	const t = useTranslations();
 	return (
 		<>
-			<RetroGrid darkLineColor="#6ea" lightLineColor="#ed199f" />
+			<RetroGrid darkLineColor="#6ea" lightLineColor="#ed199f" angle={10} />
 			<body className="bg-secondary min-h-svh max-w-svw">
 				<main className="m-5 p-2">
-					<Fade cascade damping={0.5}>
-						<NeonGradientCard
-							className="animate-fade-up"
-							neonColors={{ firstColor: "#66e", secondColor: "#6ea" }}
-						>
-							<FlexDiv>
-								<TypographyH1 className="whitespace-pre-wrap" gradient>
-									{t("meTitle")}
-								</TypographyH1>
-								<Image
-									src="/img/avatar.png"
-									width={200}
-									height={200}
-									alt="Picture of the author"
-								/>
-							</FlexDiv>
-						</NeonGradientCard>
+					<Fade delay={500}>
+						<Card>
+							<CardContent>
+								<FlexDiv>
+									<TypographyH1 className="whitespace-pre-wrap" gradient>
+										{t("meTitle")}
+									</TypographyH1>
+									<Image
+										src="/img/avatar.png"
+										width={200}
+										height={200}
+										alt="Picture of the author"
+									/>
+								</FlexDiv>
+							</CardContent>
+						</Card>
 					</Fade>
 					<ModeToggle />
 				</main>
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,76 +1,105 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
+interface GradientProps {
+	middle: string;
+	start: string;
+	end: string;
+}
 const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "rounded-xl border bg-card text-card-foreground shadow",
-      className
-    )}
-    {...props}
-  />
-))
-Card.displayName = "Card"
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement> & { gradient?: GradientProps }
+>(
+	(
+		{
+			className,
+			gradient = {
+				start: "teal",
+				middle: "blue",
+				end: "purple",
+			},
+			...props
+		},
+		ref,
+	) => (
+		<div
+			className={`w-full rounded-md bg-gradient-to-r shadow-lg shadow-teal-500/50 from-${gradient.start}-500 via-${gradient.middle}-500 to-${gradient.end}-500 p-1`}
+		>
+			<div
+				ref={ref}
+				className={cn(
+					"rounded-xl border bg-card text-card-foreground shadow b",
+					className,
+				)}
+				{...props}
+			/>
+		</div>
+	),
+);
+Card.displayName = "Card";
 
 const CardHeader = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
-    {...props}
-  />
-))
-CardHeader.displayName = "CardHeader"
+	<div
+		ref={ref}
+		className={cn("flex flex-col space-y-1.5 p-6", className)}
+		{...props}
+	/>
+));
+CardHeader.displayName = "CardHeader";
 
 const CardTitle = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("font-semibold leading-none tracking-tight", className)}
-    {...props}
-  />
-))
-CardTitle.displayName = "CardTitle"
+	<div
+		ref={ref}
+		className={cn("font-semibold leading-none tracking-tight", className)}
+		{...props}
+	/>
+));
+CardTitle.displayName = "CardTitle";
 
 const CardDescription = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
-    {...props}
-  />
-))
-CardDescription.displayName = "CardDescription"
+	<div
+		ref={ref}
+		className={cn("text-sm text-muted-foreground", className)}
+		{...props}
+	/>
+));
+CardDescription.displayName = "CardDescription";
 
 const CardContent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
-))
-CardContent.displayName = "CardContent"
+	<div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+));
+CardContent.displayName = "CardContent";
 
 const CardFooter = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+	HTMLDivElement,
+	React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn("flex items-center p-6 pt-0", className)}
-    {...props}
-  />
-))
-CardFooter.displayName = "CardFooter"
+	<div
+		ref={ref}
+		className={cn("flex items-center p-6 pt-0", className)}
+		{...props}
+	/>
+));
+CardFooter.displayName = "CardFooter";
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export {
+	Card,
+	CardHeader,
+	CardFooter,
+	CardTitle,
+	CardDescription,
+	CardContent,
+};
