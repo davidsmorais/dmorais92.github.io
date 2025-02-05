28214b4c8f85b3e6792650fc3ef673fdea9702f8
David  Morais <davidmorais92@gmail.com>
2025-02-05 15:59:21 +0000

    🚧wip: modules for home page
        by David Morais <david@davidmorais.com>

---
+++ src/components/modules/FindMe.tsx
@@ -0,0 +1,9 @@
+const FindMe = () => {
+	return (
+		<div>
+			<h1>Find Me</h1>
+		</div>
+	);
+};
+
+export default FindMe;
---
+++ src/components/modules/TechStack.tsx
@@ -0,0 +1,10 @@
+const TechStack = () => {
+	return (
+		<div>
+			<h1>About Me</h1>
+			<p>My name is John Doe and I'm a software engineer.</p>
+		</div>
+	);
+};
+
+export default TechStack;
