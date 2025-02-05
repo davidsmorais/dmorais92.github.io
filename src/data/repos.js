89b6f6b865e1ee7d99b1fb30cce0da724b84bab4
David  Morais <davidmorais92@gmail.com>
2025-02-05 20:22:37 +0000

    🐛fix: data path and alias
        by David Morais <david@davidmorais.com>

---
+++ src/data/repos.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    label: "Kuro 🖤",
+    repo: "davidsmorais/kuro",
+    description:
+      "Kuro is an unofficial, featureful, open source, community-driven, free Microsoft To-Do app for Linux with Dark Theme support and much more",
+    logoUrl:
+      "https://raw.githubusercontent.com/davidsmorais/kuro/master/static/Icon.png",
+  },
+  {
+    label: "Epoch Rift 🟢",
+    description:
+      "A roguelike platformer featuring wizards 🧙 Built with Phaser JS, releasing for PC somewhere along the road.",
+    logoUrl: "https://epochrift.com/images/riftupscaled.gif",
+    link: "https://epochrift.com",
+  },
+  {
+    label: "Dark Magic Themes 🎨",
+    description:
+      "A set of original dark themes for VSCode including custom versions of popular themes like Tokyo Night and Dracula",
+    logoUrl:
+      "https://davidmorais.gallerycdn.vsassets.io/extensions/davidmorais/dark-magic-themes/0.1.2/1670264988633/Microsoft.VisualStudio.Services.Icons.Default",
+    link: "https://marketplace.visualstudio.com/items?itemName=DavidMorais.dark-magic-themes",
+  },
+  {
+    label: "React Bifrost ❄️",
+    repo: "Dark-Magic-Studios/react-bifrost",
+    description:
+      "A React library that bridges multiple frameworks/events inside the same app. Works with Phaser JS Games ✨",
+    logoUrl:
+      "https://raw.githubusercontent.com/Dark-Magic-Studios/react-bifrost/HEAD/docs/logo.png",
+  },
+  {
+    label: "phaser-bifrost-vite-template-ts 🏗️",
+    repo: "Dark-Magic-Studios/phaser-bifrost-vite-template-ts ",
+    description:
+      "A template for Phaser + Typescript, compiled with Vite. Includes support for React Bifrost and other built-in goodies",
+  },
+];
---
+++ src/data/skillsTerminal.js
@@ -0,0 +1,303 @@
+export default [
+  {
+    title: "Front-End",
+    mainSkills: [
+      {
+        title: "JavaScript",
+        stack: [
+          "React",
+          "Redux",
+          "React Native",
+          "Electron",
+          "D3.js",
+          "jQuery",
+          "Typescript",
+        ],
+      },
+      {
+        title: "CSS",
+        stack: ["Bootstrap", "LESS", "Sass", "Styled Components"],
+      },
+      {
+        title: "GameDev",
+        stack: ["PhaserJS", "Aseprite", "Typescript"],
+      },
+      {
+        title: "Other",
+        stack: [
+          "Webpack",
+          "CRA",
+          "Material UI",
+          "Grommet UI",
+          "Wordpress",
+          "Netlify",
+          "Gatsby",
+          "JAM Stack",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Back-End",
+    mainSkills: [
+      {
+        title: "Languages",
+        stack: ["Python", "Ruby", "Node JS", "PHP"],
+      },
+      {
+        title: "Databases",
+        stack: ["PostgreSQL", "MongoDB"],
+      },
+      {
+        title: "Frameworks",
+        stack: ["Node ~ Express", "Python ~ Flask", "Ruby ~ Rails"],
+      },
+      {
+        title: "Other",
+        stack: [
+          "CircleCI",
+          "CRA",
+          "Material UI",
+          "Grommet UI",
+          "Wordpress",
+          "Netlify",
+          "Gatsby",
+          "JAM Stack",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Design",
+    mainSkills: [
+      {
+        title: [
+          "I can deliver prototypes for web and mobile apps.",
+          "I can deliver all kinds of graphics for your project including media kits and branding",
+        ],
+      },
+      {
+        title: "Software",
+        stack: [
+          "Figma",
+          "Affinity Designer / Adobe Illustrator",
+          "Affinity Photo / Adobe Photoshop",
+          "Krita",
+          "GIMP",
+        ],
+      },
+      {
+        title: "Other",
+        stack: ["Prototyping", "Wireframing", "Social Media Kit", "Branding"],
+      },
+    ],
+  },
+  {
+    title: "Projects & Work",
+    clickables: {
+      work: [
+        {
+          type: "title",
+          content: "Pennylane",
+        },
+        {
+          type: "label",
+          content: "dec 2021 ~ present",
+        },
+        {
+          type: "title",
+          content: "Gaspard+Bruno",
+        },
+        {
+          type: "label",
+          content: "jan 2020 - dec 2021",
+        },
+        {
+          type: "title",
+          content: "Intrasurance Group",
+        },
+        {
+          type: "label",
+          content: "sep 2018 - jan 2020",
+        },
+        {
+          type: "title",
+          content: "Frames",
+        },
+        {
+          type: "label",
+          content: "apr 2017 - sep 2018",
+        },
+        {
+          type: "spacer",
+        },
+        {
+          type: "label",
+          link: "https://www.linkedin.com/in/dmorais92/",
+          content: "for more info =>",
+          icon: "linked-in.svg",
+        },
+      ],
+      mdyna: [
+        {
+          type: "title",
+          content: "MDyna (Legacy)",
+        },
+        {
+          type: "label",
+          link: "https://mdyna.netlify.app",
+          content: "mdyna.netlify.app",
+        },
+        {
+          type: "label",
+          content:
+            "MDyna is a markdown notes, cross-platform app, with GitHub Sync",
+        },
+        {
+          type: "image",
+          src: "mdyna.png",
+        },
+        {
+          type: "emoji",
+          content: "🔨",
+        },
+        {
+          type: "label",
+          content:
+            "Electron, React, Circle CI, Grommet UI Webpack, Redux, Github Actions, Netlify",
+        },
+      ],
+      "farn-l": [
+        {
+          type: "title",
+          content: "Le Farn L",
+        },
+        {
+          type: "label",
+          link: "https://farn-l.netlify.com",
+          content: "farn-l.netlify.com",
+        },
+        {
+          type: "label",
+          content:
+            "This is a food menus service with an API and a backoffice, serving restaurant websites with daily menus.",
+        },
+        {
+          type: "image",
+          src: "farnl.svg",
+        },
+        {
+          type: "emoji",
+          content: "🔨",
+        },
+        {
+          type: "label",
+          content:
+            "React, Styled Components, Netlify, Ruby on Rails, Heroku, PostgreSQL",
+        },
+      ],
+      "epoch-rift": [
+        {
+          type: "title",
+          content: "Epoch Rift",
+        },
+        {
+          type: "label",
+          link: "https://epochrift.com",
+          content: "epochrift.com",
+        },
+        {
+          type: "label",
+          content:
+            "Pixel art roguelike game with a fantasy theme. Play as a wizard or a witch in this fun platformer game.",
+        },
+        {
+          type: "image",
+          src: "epochrift.png",
+        },
+        {
+          type: "emoji",
+          content: "🔨",
+        },
+        {
+          type: "label",
+          content: "Typescript, PhaserJS, Webpack, Electron, Pixel Art",
+        },
+      ],
+      kuro: [
+        {
+          type: "title",
+          content: "Kuro",
+        },
+        {
+          type: "label",
+          link: "https://github.com/davidsmorais/kuro/",
+          content: "🌳 GitHub",
+        },
+        {
+          type: "label",
+          content:
+            "Elegant Microsoft To-Do desktop client for Linux with dark themes and other features.",
+        },
+        {
+          type: "image",
+          src: "kuro.png",
+        },
+        {
+          type: "emoji",
+          content: "🔨",
+        },
+        {
+          type: "label",
+          content: "Electron, Javascript, CSS, Node",
+        },
+      ],
+    },
+    mainSkills: [
+      {
+        title: "Projects",
+        stack: ["Epoch Rift", "MDyna 💀", "Le Farn L 💀", "Kuro"],
+        clickableKeys: [
+          {
+            label: "Epoch Rift",
+            key: "epoch-rift",
+          },
+          {
+            label: "Kuro",
+            key: "kuro",
+          },
+          {
+            label: "MDyna 💀",
+            key: "mdyna",
+          },
+          {
+            label: "Le Farn L 💀",
+            key: "farn-l",
+          },
+        ],
+      },
+      {
+        title: "Work",
+        stack: ["Pennylane", "Gaspard + Bruno", "Intrasurance", "Frames"],
+        clickableKeys: [
+          {
+            label: "Pennylane",
+            key: "work",
+          },
+          {
+            label: "Gaspard + Bruno",
+            key: "work",
+          },
+          {
+            label: "Intrasurance",
+            key: "work",
+          },
+          {
+            label: "Frames",
+            key: "work",
+          },
+        ],
+      },
+    ],
+  },
+];
